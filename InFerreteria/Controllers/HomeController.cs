87f1e36905ea4bd9eeb35bf124b5ee33ffe4db4c
lacarrillo2001 <160196533+NRC3788-lacarrillo6@users.noreply.github.com>
2025-10-26 23:23:37 -0500

    Vistas

--- InFerreteria/Controllers/HomeController.cs
+++ InFerreteria/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
-using System.Diagnostics;
 using InFerreteria.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Data;
 
 namespace InFerreteria.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext db, ILogger<HomeController> logger)
         {
+            _db = db;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            if (User?.Identity?.IsAuthenticated == true)
+            {
+                ViewBag.ArticulosActivos = _db.Articulos.Count(a => a.Activo);
+                ViewBag.ArticulosInactivos = _db.Articulos.Count(a => !a.Activo);
+                ViewBag.Categorias = _db.Categorias.Count();
+                // Dentro de Index(), solo si está autenticado:
+                ViewBag.Proveedores = _db.Proveedores.Count();
+
+            }
             return View();
         }
 
